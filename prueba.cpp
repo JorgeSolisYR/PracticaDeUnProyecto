dc888be0250b0acc9bb621f05e8dedbed7ef0caa
JorgeSolisYR <61199290+JorgeSolisYR@users.noreply.github.com>
2020-02-24 21:23:41 -0600

    Add files via upload

---
+++ prueba.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+class Rectangulo{
+ private:
+  float largo;
+  float ancho;
+ public:
+  Rectangulo(float,float);
+  void Perimetro();
+  void Area();
+};
+
+Rectangulo::Rectangulo(float L,float A){
+ largo=L;
+ ancho=A;
+}
+
+void Rectangulo::Perimetro(){
+ cout<<"El Perimetro es:"<<(2*largo+2*ancho)<<endl;
+}
+
+void Rectangulo::Area(){
+ cout<<"El Area Del Rectangulo Es De:"<<(largo*ancho)<<endl;
+}
+
+int main(){
+ Rectangulo R1(7,6);
+ R1.Perimetro();
+ R1.Area();
+}
